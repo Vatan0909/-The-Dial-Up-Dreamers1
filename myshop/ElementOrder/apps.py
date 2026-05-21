d595839fff80ae7f7c094225131d91a0c5518366
MohammadAminAlinasab <lynsbmhmd8@gmail.com>
2026-05-21 17:09:10 +0330

    chore: initialize ElementOrder app

---
+++ myshop/ElementOrder/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ElementorderConfig(AppConfig):
+    name = 'ElementOrder'
--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'Cart',
     'Orders',
     'Recommender',
-    'Users'
+    'Users',
+    'ElementOrder',
     
 ]
 
