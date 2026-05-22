9ea2dda5205b5a98ef11359be1b236f539e44998
MohammadAminAlinasab <lynsbmhmd8@gmail.com>
2026-05-22 19:34:34 +0330

    feat: add Profile and Address tables to database

--- myshop/Users/models.py
+++ myshop/Users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    # making one to one relationship between profile model and User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    phone = models.CharField(max_length=11, null = False, blank = False, unique = True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Address(models.Model):
+    # every user can have multiple addresses
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses')
+    title = models.CharField(max_length=50, verbose_name="address title")
+    address_text = models.TextField(verbose_name="detailed address")
+    postal_code = models.CharField(max_length=10, blank=True, null=True)
+    is_default = models.BooleanField(default=False, verbose_name="is default address")
+
+    def __str__(self):
+        return f"{self.user.username} - {self.title}"
