52981ee2c04d3ae02e148e0f7b075260d7d75702
MohammadAminAlinasab <lynsbmhmd8@gmail.com>
2026-06-23 22:00:42 +0330

    fix: add mimetypes import to ensure JavaScript files load properly on Windows

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+import mimetypes
 
+mimetypes.add_type("application/javascript", ".js", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
